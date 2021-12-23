a51864aeaaf614aa3e72500a07f75e0fd5d1d461
Khamza <habibullahkhamza@gmail.com>
2021-12-23 11:17:50 +0400

    learning gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
+import { ExampleButton } from "../components/button"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <h1>Home Page</h1>
+      <ExampleButton>click me</ExampleButton>
+    </Layout>
+  )
 }
